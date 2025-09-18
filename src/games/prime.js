0b10b7f59f2add467b831ccd441dda8b77096bc5
Perfectblue1710 <perfectblueee00@gmail.com>
2025-09-18 11:57:25 +0500

    исправления

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import runEngine from '../index.js';
+import getRandomNumber from '../utils.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getRound = () => {
+  const number = getRandomNumber(2, 100); 
+  const question = String(number);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => runEngine(description, getRound);
